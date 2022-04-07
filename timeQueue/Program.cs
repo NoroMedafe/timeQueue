c717ca96cfef61c936e77e33edf4c909265b3e3a
NoroMedafe <danya1515@inbox.ru>
2022-04-07 16:28:30 +0300

    Добавьте файлы проекта.

---
+++ timeQueue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timeQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeOfReceipt = 10;
+            int queueLenght;
+            const int hour = 60;
+            int timeInLine;
+            int countHours;
+            int countMinutes;
+
+            Console.Write("Введите количество людей в очереди: "); queueLenght = Convert.ToInt32(Console.ReadLine());
+            timeInLine = timeOfReceipt * queueLenght;
+            countHours = timeInLine / hour;
+            countMinutes = timeInLine % hour;
+            Console.WriteLine($"Вы должны отстоять в очереди {countHours} часа и {countMinutes} минут");
+            Console.ReadKey();
+        }
+    }
+}
